6af3af488ab91115ea09756c973532b2664dab2f
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-23 11:25:48 -0800

    added stock to item.spec test

--- __tests__/item.spec.js
+++ __tests__/item.spec.js
@@ -2,8 +2,8 @@ const Item = require('../lib/item');
 
 describe('item', () => {
   test('Create a new item in vending machine with a name and price', () => {
-    const vendingItem = new Item('mars bar', 2.35);
-    expect(vendingItem).toEqual({ name: 'mars bar', price: 2.35 });
+    const vendingItem = new Item('mars bar', 2.35, 4);
+    expect(vendingItem).toEqual({ name: 'mars bar', price: 2.35, stock: 4 });
   });
   test('');
 });
