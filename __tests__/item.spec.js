86ae2e622c5c4bdfa7cbd36b45263d0a56f0fa6a
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-02-23 18:50:30 -0800

    added two more stock tests

--- __tests__/item.spec.js
+++ __tests__/item.spec.js
@@ -1,9 +1,26 @@
 const Item = require('../lib/item');
 
 describe('item', () => {
-  test('Create a new item in vending machine with a name, price, and stock', () => {
-    const vendingItem = new Item('mars bar', 2.35, 4);
-    expect(vendingItem).toEqual({ name: 'mars bar', price: 2.35, stock: 4 });
+  let item;
+  let marsBar;
+  let pretzels;
+  let sunChips;
+  beforeEach(() => {
+    item = new Item();
+    marsBar = new Item('mars bar', 2.5, 4);
+    pretzels = new Item('pretzels', 3, 2);
+    sunChips = new Item('sun chips', 2.5, 0);
   });
-  test('');
+  test('Create a new item in vending machine with a name, price, and stock', () => {
+    const vendingItem = marsBar;
+    expect(vendingItem).toEqual({ name: 'mars bar', price: 2.5, stock: 4 });
+  });
+  test('Check if this item is still in stock', () => {
+    const result = marsBar.checkStock();
+    expect(result).toEqual('This item is in');
+  });
+  test('If this is out of stock, return out of stock', () => {
+    const result = sunChips.checkStock();
+    expect(result).toEqual('This item is out of stock');
+  });
 });
